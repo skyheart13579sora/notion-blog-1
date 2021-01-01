3ce9703023c050c1de4585771341003a2b19fa28
Sora Taniguchi <32994482+skyheart13579sora@users.noreply.github.com>
2021-01-01 22:09:03 +0900

    Update index.tsx
    ブログの構成学び

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,24 +31,8 @@ export default () => (
 
       <div className="explanation">
         <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
-
-        <p>
+        日頃の気付きや学びなど
+        思いついたことをつらつら書いていくブログ
          
         </p>
       </div>
