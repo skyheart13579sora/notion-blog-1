1da29bd895855c180031b028723d27fd9ba2c70e
skyheart235711 <skyheart235711@outlook.jp>
2021-01-01 23:43:11 +0900

    master

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,14 +3,16 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
+      <div>
       <span>
-      Powered by {' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          Notion Blog
-        </ExtLink>
-      </span>
+        Powered by {' '}
+          <ExtLink href="https://github.com/ijjk/notion-blog">
+            Notion Blog
+          </ExtLink>
+        </span>
+      </div>
       
-      <span class = "br">
+      <span>
       Copyright © 2021 {' '}
         <ExtLink href="https://twitter.com/Sky_MouthValley">
           Sora Taniguchi
